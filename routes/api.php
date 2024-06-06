b26162402027817d2d0a120678ef006bfb764173
A.Zhukov <lozemc@gmail.com>
2024-06-06 09:14:07 +0300

    Убран лишний код в api.php

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::group(['prefix' => 'v1'], static function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
@@ -23,7 +18,3 @@ Route::group(['prefix' => 'v1'], static function () {
         Route::get('chats/{chat}/messages', [ChatController::class, 'getMessages']);
     });
 });
-
-Route::any('{any}', static function () {
-    return response()->json(['status' => false, 'errors' => 'Method not found'], 400);
-})->where('any', '.*');
