eb204e7e0aca97087bb2b273b572fc4299a4b8e4
A.Zhukov <lozemc@gmail.com>
2024-06-06 09:13:20 +0300

    Создание документации по API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AuthRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(AuthRequest $request): \Illuminate\Http\JsonResponse
+    /**
+     * Авторизация
+     * @bodyParam email string required E-mail пользователя. Example: example@gmail.com
+     * @bodyParam password string required Пароль пользователя. Example: s7Sjkd6fg230d
+     * @response {"status": true,"response": {"accessToken": "1|g2YrOcIVUWaI3JHinARFSYiFj4YCJyIxWp14JO2B024b58s9"}}
+     * @response 401 {"status": false, "errors": "Email & Password does not match with our record"}
+     */
+    public function login(Request $request): JsonResponse
     {
+        $validator = validator($request->all(), [
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->returnJson(status: false, errors: $validator->messages()->all(), code: 401);
+        }
+
         if (Auth::attempt($request->only(['email', 'password']), true)) {
             $user = $request->user();
             $user->tokens()->delete();
--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
+    /**
+     * Создание чата с пользователем
+     * @authenticated
+     * @bodyParam userId string required Идентификатор чата собеседника. Example: 5
+     * @response {"status": true,"data": {"chatId": 123}}
+     * @response 400 {"status":false,"errors":{"userId":["The selected user id is invalid."]}}
+     */
     public function createChat(Request $request): JsonResponse
     {
         $validator = validator($request->all(), ['userId' => 'required|integer|exists:users,id']);
@@ -35,6 +42,11 @@ class ChatController extends Controller
         return $this->returnJson(true, ['chatId' => $chat->id]);
     }
 
+    /**
+     * Получение списка чатов
+     * @authenticated
+     * @response {"status":true,"response":{"current_page":1,"data":[{"chatId":2,"timestamp":1717617112,"chatName":"Чат с Alyson Monahan","users":[{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"},{"id":3,"firstName":"Alyson","lastName":"Monahan","email":"271katlynn69@example.net"}]},{"chatId":1,"timestamp":1717617105,"chatName":"Чат с Audreanne Brekke","users":[{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"},{"id":2,"firstName":"Audreanne","lastName":"Brekke","email":"294kris.cassandra@example.com"}]},{"chatId":2,"timestamp":1717617112,"chatName":"Чат с Alyson Monahan","users":[{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"},{"id":3,"firstName":"Alyson","lastName":"Monahan","email":"271katlynn69@example.net"}]},{"chatId":1,"timestamp":1717617105,"chatName":"Чат с Audreanne Brekke","users":[{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"},{"id":2,"firstName":"Audreanne","lastName":"Brekke","email":"294kris.cassandra@example.com"}]},{"chatId":1,"timestamp":1717617105,"chatName":"Чат с Audreanne Brekke","users":[{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"},{"id":2,"firstName":"Audreanne","lastName":"Brekke","email":"294kris.cassandra@example.com"}]}],"first_page_url":"http://localhost/api/v1/chats?page=1","from":1,"last_page":1,"last_page_url":"http://localhost/api/v1/chats?page=1","links":[{"url":null,"label":"&laquo; Previous","active":false},{"url":"http://localhost/api/v1/chats?page=1","label":"1","active":true},{"url":null,"label":"Next &raquo;","active":false}],"next_page_url":null,"path":"http://localhost/api/v1/chats","per_page":20,"prev_page_url":null,"to":5,"total":5}}
+     */
     public function listChats(): JsonResponse
     {
         $currentUser = auth()->id();
@@ -74,8 +86,22 @@ class ChatController extends Controller
         return $this->returnJson(true, $chats);
     }
 
+    /**
+     * Отправка сообщений
+     * @authenticated
+     * @UrlParam chat_id int required Идентификатор чата. Example: 2
+     * @bodyParam message string required Текст сообщения. Example: Text message
+     * @response {"status":true}
+     * @response 400 {"status":false,"errors":{"message":["The message field is required."]}}
+     */
     public function sendMessage(Chat $chat, Request $request): JsonResponse
     {
+        $validator = validator($request->all(), ['message' => 'required|string']);
+
+        if ($validator->fails()) {
+            return $this->returnJson(status: false, errors: $validator->errors()->toArray(), code: 400);
+        }
+
         $chat->messages()->create([
             'user_id' => auth()->id(),
             'message' => $request->input('message'),
@@ -84,6 +110,12 @@ class ChatController extends Controller
         return $this->returnJson(true);
     }
 
+    /**
+     * Получение списка сообщений
+     * @authenticated
+     * @UrlParam chat_id int required Идентификатор чата. Example: 2
+     * @response {"status":true,"response":{"current_page":1,"data":[{"messageId":5,"timestamp":1717652259,"text":"Text message","user":{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"}},{"messageId":3,"timestamp":1717617341,"text":"three","user":{"id":1,"firstName":"Ivanov","lastName":"Ivan","email":"ivan@ya.ru"}}],"first_page_url":"http://localhost/api/v1/chats/2/messages?page=1","from":1,"last_page":1,"last_page_url":"http://localhost/api/v1/chats/2/messages?page=1","links":[{"url":null,"label":"&laquo; Previous","active":false},{"url":"http://localhost/api/v1/chats/2/messages?page=1","label":"1","active":true},{"url":null,"label":"Next &raquo;","active":false}],"next_page_url":null,"path":"http://localhost/api/v1/chats/2/messages","per_page":2,"prev_page_url":null,"to":2,"total":2}}
+     */
     public function getMessages(Chat $chat): JsonResponse
     {
         $messages = $chat->messages()
